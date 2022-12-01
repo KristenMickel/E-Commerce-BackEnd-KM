c4ba1d1dd20618cd96deb6301df11f328bbadd74
unknown <mickel.kristen@gmail.com>
2022-12-01 06:41:45 -0500

    Updated category-routes.js file & started working on product-routes.js file

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -1,21 +1,48 @@
 const router = require('express').Router();
-const { Product, Category, Tag, ProductTag } = require('../../models');
+const { Product, Category, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: ProductTag }],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    //We are saying that I want to find a driver by the primary key (.findByPk is another sequelize method)
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: ProductTag }], //Here, we are saying that I also want you to include data from the License table and the Car table along with the driver data. The result is that we end up with an object that has a single license and multiple cars.
+      //Can also do the following where you specify which columns and if they are required (which will drive what kind of JOIN is used).
+      //include: [{ model: License, attributes: [''], required: }, { model: Car}],
+      //We define a relationship and then based on how that relationship is defined, sequelize knows how to query the database and get the data and bring it back to us.
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
+//This provided code is not using async.
 router.post('/', (req, res) => {
   /* req.body should look like this...
     {
@@ -25,7 +52,12 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+  Product.create({
+    product_name: "Blue T-Shirt",
+    price: 15.00,
+    stock: 8,
+    tagIds: [3]
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -89,8 +121,15 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  const productData = await Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  return res.json(productData);
 });
 
 module.exports = router;
